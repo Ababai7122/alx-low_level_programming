1eea9365343a0ba9a81cd5971fb07a12df85bf12
Ababai7122 <anasabake@gmail.com>
2023-08-15 03:04:10 -0700

    print characters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet, in lowercase, followed by a new line
+ *
+ * Returns 0 Alway (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	
+	while(ch <= 'z');
+
+	printf ("ch\n");
+	
+	return (0);
+{
