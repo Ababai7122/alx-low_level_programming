5ee661e3e4ce286e1dfba59ea31f409c4a7a4d9a
Ababai7122 <anasabake@gmail.com>
2023-08-17 07:39:49 -0700

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * _isupper - check if c is upper
+ *
+ * @c: inpt for alphabet
+ *
+ * Return: 1 if its uppercase and 0 if lowercase
+ *
+ */
+
+int _isupper(int c);
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
